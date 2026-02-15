7241d785fd709499857f838e260b52404143b6be
AdaKirikkalp <akirikkalp@gmail.com>
2026-02-15 21:16:43 +0300

    Add files via upload

---
+++ M1L2.py
@@ -0,0 +1,26 @@
+#import random
+#karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+#uzunluk = int(input("Şifrenizin uzunluğunu giriniz"))
+
+#sifre = ""
+
+#for i in range(uzunluk):
+#    karakter = random.choice(karakterler)
+ #   sifre += karakter
+#print(sifre)
+
+
+
+
+import random
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+uzunluk = int(input("Şifrenizin uzunluğunu giriniz"))
+
+sifre = []
+
+for i in range(uzunluk):
+    karakter = random.choice(karakterler)
+    sifre.append(karakter)
+print(sifre)
